a1fc82c74238ccfc627f4924cdff79630d79aaaf
Devan Lai <devan.lai@gmail.com>
2017-03-11 14:50:58 -0800

    fixup! Fix hang when disconnecting USB

--- util/NonBlockingUSBSerial.cpp
+++ util/NonBlockingUSBSerial.cpp
@@ -5,10 +5,12 @@ NonBlockingUSBSerial::NonBlockingUSBSerial(uint16_t vendor_id, uint16_t product_
 
 }
 
-bool
-NonBlockingUSBSerial::writeBlockNB(uint8_t* buf, uint16_t size) {
-    if (size <= MAX_PACKET_SIZE_EPBULK) {
-        return USBDevice::writeNB(EPBULK_IN, buf, size, MAX_PACKET_SIZE_EPBULK);
+bool NonBlockingUSBSerial::writeBlockNB(uint8_t* buf, uint16_t size) {
+    if (size <= MAX_PACKET_SIZE_EPBULK && configured()) {
+        /* Return true if the data was enqueued successfully
+           Enqueued data may be lost if the host disconnects, but data
+           will almost certainly be lost in that case anyways */
+        return (endpointWrite(EPBULK_IN, buf, size) == EP_PENDING);
     }
     return false;
 }
