2b67494515e7a727f296edccc90c3a1dfccc5a55
Rami Hijab <ramihijab@berkeley.edu>
2019-09-08 16:58:10 -0700

    2019 CAN Controller Blinky, change pin outs for each brain

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#define CAN_RX P0_29
+#define CAN_TX P0_28
+#define RX_ID 0x42
+#define TX_ID 0x40
 #include "mbed.h"
 
 #include "ledutils.h"
@@ -6,15 +10,39 @@ PwmOut ledR(P0_5);
 PwmOut ledG(P0_6);
 PwmOut ledB(P0_7);
 
-DigitalOut led1(P0_3);
-DigitalOut led2(P0_9);
+DigitalOut led2(P0_3);
+DigitalOut led1(P0_9);
 
 DigitalIn btn(P0_4);
 
 RawSerial serial(P0_8, NC, 115200);
 
+CAN can(CAN_RX, CAN_TX);
+Ticker canTicker;
+Timer blinkTimer;
+
+void sendCAN() {
+    uint8_t data[1];
+    if (!btn) {
+      data[0] = 1;
+    } else {
+      data[0] = 0;
+    }
+    can.write(CANMessage(TX_ID, (char*)data, 1));
+    led2 = !led2.read();
+}
+
 int main() {
-  ledR.write(1);
-  ledG.write(1);
-  ledB.write(1);
+  blinkTimer.start();
+  canTicker.attach(&sendCAN, 0.25);
+  can.frequency(1000000);
+  /* BRAIN 1 */
+  while (true) {
+    CANMessage msg;
+    if (can.read(msg)) {
+      if (msg.id == RX_ID) {
+        led1 = msg.data[0];
+      } 
+    }
+  }
 }
