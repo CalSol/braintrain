03f96a29886eaa3c95a0c493a61cb5fdc8db7dee
Chris Berthelet <chrisb@boostedboard.com>
2020-03-05 08:56:24 -0800

    Adding last changes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-
 #include "ledutils.h"
 
 PwmOut ledR(P0_5);
@@ -13,8 +12,52 @@ DigitalIn btn(P0_4);
 
 RawSerial serial(P0_8, NC, 115200);
 
-int main() {
-  ledR.write(1);
-  ledG.write(1);
-  ledB.write(1);
+bool whichLED;
+
+int main() 
+{
+	serial.puts("\r\n\r\n" __FILE__ ", built " __DATE__ " " __TIME__ "\r\n");
+
+	whichLED = 0;
+	led1.write(1);
+	led2.write(0);
+
+	while (true) 
+	{
+		if (btn) 
+		{
+			// ONLY switch if button NOT pressed
+			if (whichLED) 
+			{
+				// Last state was led2 ON so switch
+				led1.write(1);
+				led2.write(0);
+				whichLED = 0;
+			}
+			else 
+			{
+				// Last state was led1 ON so switch
+				led1.write(0);
+				led2.write(1);
+				whichLED = 1;
+			}
+			wait(0.5);
+			serial.printf("led1 value is %u\r\n", (int)led1);
+			serial.printf("led2 value is %u\r\n", (int)led2);
+		}
+	}
 }
+
+//Simpler Solution
+// int main() {
+//   while (true) {
+//     led1 = 1;
+//     led2 = 0;
+//     wait(0.5);
+//     while(!btn);
+//     led1 = 0;
+//     led2 = 1;
+//     wait(0.5);
+//     while(!btn);
+//   }
+// }
