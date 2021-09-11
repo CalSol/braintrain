2db55feedf03362850f0d60b5a6313550bf3d356
Richard Lin <elpato25@gmail.com>
2021-09-10 23:16:23 -0700

    kinda doing ... not much actually =(

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,28 @@
 DigitalOut led1(LED1);
 
 // Replace pin assignments if different
-PwmOut ledR(PA_11);
-PwmOut ledG(PB_5);
-PwmOut ledB(PB_4);
+PwmOut ledR(D10);
+PwmOut ledG(D11);
+PwmOut ledB(D12);
 
-DigitalIn btn(PA_8, PullUp);
+DigitalIn btn(D13, PullUp);
 
 RawSerial serial(SERIAL_TX, SERIAL_RX, 115200);
 
 int main() {
+  while(1) {
+  led1.write(1);
+  
   ledR.write(0);
   ledG.write(0);
   ledB.write(0);
+  
+  wait(0.5);
+  
+  led1.write(0);
+  
+  ledR.write(1);
+  ledG.write(1);
+  ledB.write(1);
+  }
 }
